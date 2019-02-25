6ec9a9c999ae68f027230dee3bdc7556dab8ccda
Daan Hoogland <daan@hoogland.io>
2019-02-25 13:05:44 +0100

    Updated polybar launch script

--- polybar/init.sh
+++ polybar/init.sh
@@ -7,5 +7,5 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main & polybar secondary & polybar tray
+polybar main & polybar secondary
 echo "Bars launched..."
