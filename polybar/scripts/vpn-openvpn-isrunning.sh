0f1b61a008d55618a78651a6e95a71a9208a7a96
Daan Hoogland <daan@hoogland.io>
2019-02-25 09:38:14 +0100

    Added notification sounds and updated polybar conf

--- polybar/scripts/vpn-openvpn-isrunning.sh
+++ polybar/scripts/vpn-openvpn-isrunning.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$(systemctl | grep openvpn | grep running)" ]; then
-    echo "%{F#AB9C71}%{F-}"
+    echo "%{F#AB9C71}%{F-}"
 else
-    echo "%{F#33}%{F-}"
+    echo "%{F#33}%{F-}"
 fi
