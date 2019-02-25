668a56d6de1f8a46206aea3c0e4b7e88784f9f43
Daan Hoogland <daan@hoogland.io>
2019-02-25 13:05:09 +0100

    Updated Polybar tray configuration

--- i3/scripts/commitreminder.sh
+++ i3/scripts/commitreminder.sh
@@ -2,7 +2,7 @@
 while true ; do
 	if ps aux | grep "[i]ntelli" > /dev/null
 	then
-		notify-send -u critical "Commit your work!" "It's been 30 minutes since the last message, commit and push your work for safety" -i /usr/share/icons/Numix/128/actions/merge.svg
+		notify-send -u normal "Commit your work!" "It's been 30 minutes since the last message, commit and push your work for safety" -i /usr/share/icons/Numix/128/actions/merge.svg
 	fi
 	sleep 30m
 done
